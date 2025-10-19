873db82f64c9dd1e5bf08d391f52c3ed9cbda3f6
n4n5 <its.just.n4n5@gmail.com>
2025-10-18 22:09:50 -0600

    change script

--- list.sh
+++ list.sh
@@ -11,73 +11,21 @@ if [ -z "$USERNAME" ]; then
     USERNAME="$me"
 fi
 
-USER_AGENT="get all crates scripts"
-
-PAGE=1
-PER_PAGE=100
-ALL_CRATES=()
-
-user_id=$(curl -s "https://crates.io/api/v1/users/$USERNAME" -H "User-Agent: $USER_AGENT" | jq -r '.user.id')
-
-if [ -z "$user_id" ] || [ "$user_id" == "null" ]; then
-    echo "User '$USERNAME' not found on crates.io."
-    exit 1
-fi
-
-echo "Fetching crates for user '$USERNAME' (ID: $user_id)..."
-
-while : ; do
-    sleep 1  # To avoid hitting the API too fast
-    RESPONSE=$(curl -s "https://crates.io/api/v1/crates?user_id=$user_id&page=$PAGE&per_page=$PER_PAGE" -H "User-Agent: $USER_AGENT")
-
-    # Check if the response contains crates
-    CRATES=$(echo "$RESPONSE" | jq -r '.crates[].id')
-    if [ -z "$CRATES" ]; then
-        break
-    fi
-
-    # Append to the list
-    while IFS= read -r crate; do
-        ALL_CRATES+=("$crate")
-    done <<< "$CRATES"
-
-    # If less than requested, it's the last page
-    COUNT=$(echo "$CRATES" | wc -l)
-    if [ "$COUNT" -lt "$PER_PAGE" ]; then
-        break
-    fi
-
-    ((PAGE++))
-done
-
-# Print results
-echo "Crates published by user '$USERNAME':"
-# crate a json to list.json
-echo "[" > list.json
-for i in "${!ALL_CRATES[@]}"; do
-    echo "  \"${ALL_CRATES[$i]}\"$(if [ "$i" -lt $((${#ALL_CRATES[@]} - 1)) ]; then echo ","; fi)"
-done >> list.json
-echo "]" >> list.json
-echo "Total crates found: ${#ALL_CRATES[@]}"
-echo "Crates list saved to list.json"
-
-
 {
-echo "# crates"
-echo ""
-echo "- <https://crates.io/users/Its-Just-Nans>"
-echo -e "\n## Crates\n"
-echo "| Crate | Description | Homepage && Repo |"
-echo "|-------|------------|----------|"
+    echo "# crates"
+    echo ""
+    echo "- <https://crates.io/users/$USERNAME>"
+    echo -e "\n## Crates\n"
+    echo "| Crate | Description | Homepage && Repo |"
+    echo "|-------|------------|----------|"
 }> README.md
-for crate in "${ALL_CRATES[@]}"; do
-    sleep 0.5  # To avoid hitting the API too fast
-    CRATE_INFO=$(curl -s "https://crates.io/api/v1/crates/$crate" -H "User-Agent: $USER_AGENT")
+n4n5 utils list_crates -o - -f -d 0.5 | jq -c '.[]' | while read -r CRATE_INFO; do
+    CRATE_NAME=$(echo "$CRATE_INFO" | jq -r '.crate.name')
     REPO_URL=$(echo "$CRATE_INFO" | jq -r '.crate.repository')
     HOMEPAGE_URL=$(echo "$CRATE_INFO" | jq -r '.crate.homepage')
     DOC_URL=$(echo "$CRATE_INFO" | jq -r '.crate.documentation')
     DESCRIPTION=$(echo "$CRATE_INFO" | jq -r '.crate.description')
-    echo -n "| [$crate](https://crates.io/crates/$crate) |"
+    echo -n "| [$CRATE_NAME](https://crates.io/crates/$CRATE_NAME) |"
     if [ "$DESCRIPTION" != "null" ]; then
         echo -n "$DESCRIPTION"
     else
